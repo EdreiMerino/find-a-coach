9a3e3564b5bfb83d548e12633d952f67edac91b8
Edrei Merino <edreimerino@gmail.com>
2023-10-18 10:33:53 -0700

    Working on the Main Layout & Styling

--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 
-const router = createRouer({
+const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
